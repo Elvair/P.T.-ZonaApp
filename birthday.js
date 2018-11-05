175be1ed87cf87c8c8a230fcb0512a3f1325b1bf
Elvair <42173778+Elvair@users.noreply.github.com>
2018-11-05 04:24:13 +0100

    Create birthday.js

---
+++ birthday.js
@@ -0,0 +1,19 @@
+//const s = [2, 2, 1, 3, 2];
+//const d = 4;
+//const m = 2;
+
+
+
+let birthday = (s, d, m) => {
+    let answer = 0;
+    for (let i = 0; i < s.length; i++) {
+        let seg = 0;
+        for (let j = i; j < (m + i); j++) {
+            seg += parseInt(s[j]);
+        }
+        if (seg === d) {
+            answer += 1;
+        }
+    }
+    return (answer);
+}
